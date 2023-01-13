f7bdd4325f13bca942a8e55991ba6afbc85880f1
Nikolay Hizanishvili <nikolay.hizanishvili@gmail.com>
2023-01-13 23:29:11 +0300

    c MemStorage

--- storage/mem/mem.go
+++ storage/mem/mem.go
@@ -3,7 +3,8 @@ package mem
 import (
 	"context"
 	"crud_service/app/repos"
-	"fmt"
+	"errors"
+	"sync"
 
 	"github.com/google/uuid"
 )
@@ -12,54 +13,95 @@ var _ repos.Storage = &MemStorage{}
 
 // Runtime storage
 type MemStorage struct {
-	customers *customers
-	shops     *shops
+	sync.RWMutex
+	m map[string]map[uuid.UUID]repos.Data
 }
 
 func New() *MemStorage {
-	c := newCustomers()
-	s := newShops()
-
-	return &MemStorage{c, s}
-}
-
-func (ms *MemStorage) Create(ctx context.Context, data repos.Data) (*uuid.UUID, error) {
-	if d, ok := data.(*repos.Customer); ok {
-		return ms.customers.create(ctx, *d)
-	}
-	if d, ok := data.(*repos.Shop); ok {
-		return ms.shops.create(ctx, *d)
+	return &MemStorage{
+		m: make(map[string]map[uuid.UUID]repos.Data),
 	}
 
-	return nil, fmt.Errorf("there is no storage for this type of data")
 }
-func (ms *MemStorage) Read(ctx context.Context, data repos.Data) ([]repos.Data, error) {
-	if d, ok := data.(*repos.Customer); ok {
-		if d.Id == uuid.Nil || d.Id.String() == "" {
-			return ms.customers.readSurname(ctx, d.Surname)
+
+func (s *MemStorage) Create(ctx context.Context, d repos.Data) (*uuid.UUID, error) {
+	s.Lock()
+	defer s.Unlock()
+
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	default:
+	}
+	d.SetDefaults()
+	if d.CheckRequired() {
+		return nil, errors.New("required field is missing")
+	}
+	id := d.GetId()
+	if s.m[d.GetTypeName()] == nil {
+		s.m[d.GetTypeName()] = make(map[uuid.UUID]repos.Data)
+	}
+	s.m[d.GetTypeName()][id] = d
+	return &id, nil
+}
+func (s *MemStorage) Read(ctx context.Context, d repos.Data) (repos.Data, error) {
+	s.RLock()
+	defer s.RUnlock()
+
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	default:
+	}
+
+	if s.m[d.GetTypeName()] == nil {
+		return nil, errors.New("no record with such uuid")
+	}
+
+	data, ok := s.m[d.GetTypeName()][d.GetId()]
+
+	if !ok {
+		return nil, errors.New("no record with such uuid")
+	}
+	return data, nil
+}
+func (s *MemStorage) Delete(ctx context.Context, d repos.Data) error {
+	s.Lock()
+	defer s.Unlock()
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+	}
+
+	if s.m[d.GetTypeName()] == nil {
+		return errors.New("no record with such uuid")
+	}
+
+	if _, deleted := s.m[d.GetTypeName()][d.GetId()]; !deleted {
+		return errors.New("no record with such uuid")
+	}
+	delete(s.m[d.GetTypeName()], d.GetId())
+
+	return nil
+}
+
+func (s *MemStorage) ReadBySearchField(ctx context.Context, d repos.Data) ([]repos.Data, error) {
+	s.RLock()
+	defer s.RUnlock()
+
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	default:
+	}
+
+	data := []repos.Data{}
+	for _, elem := range s.m[d.GetTypeName()] {
+		if elem.CmpSearchField(d.GetSearchField()) {
+			data = append(data, elem)
 		}
-		val, err := ms.customers.read(ctx, d.Id)
-		return []interface{}{val}, err
 	}
-
-	if d, ok := data.(*repos.Shop); ok {
-		if d.Id == uuid.Nil || d.Id.String() == "" {
-			return ms.shops.readName(ctx, d.Name)
-		}
-		val, err := ms.shops.read(ctx, d.Id)
-		return []interface{}{val}, err
-	}
-
-	return nil, fmt.Errorf("there is no storage for this type of data")
-}
-func (ms *MemStorage) Delete(ctx context.Context, data repos.Data) error {
-	if d, ok := data.(*repos.Customer); ok {
-		return ms.customers.delete(ctx, d.Id)
-	}
-
-	if d, ok := data.(*repos.Shop); ok {
-		return ms.shops.delete(ctx, d.Id)
-	}
-
-	return fmt.Errorf("there is no storage for this type of data")
+	return data, nil
 }
