4982c0435c1c95f46b5a56dd25085d46470bcd08
Nikolay Hizanishvili <nikolay.hizanishvili@gmail.com>
2023-01-13 22:16:45 +0300

    c interface of Storage && c interface of Data

--- app/repos/repos.go
+++ app/repos/repos.go
@@ -2,14 +2,52 @@ package repos
 
 import (
 	"context"
+	"reflect"
+	"strings"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 )
 
-type Data = interface{}
+// Interface of storage data
+type Data interface {
+	CheckRequired() bool
+	CmpSearchField(data string) bool
+	GetId() uuid.UUID
+	GetTypeName() string
+	GetSearchField() string
+	GetSearchFieldName() string
+	SetDefaults()
+	DbData() DbData
+}
 
+// Interface of database allowed data
+type DbData interface {
+	// Sets fields of DbData by reading database row
+	SetFieldsFromDbRow(ctx context.Context, row pgx.Rows) error
+	// Inserts DbData via database connection
+	Insert(ctx context.Context, connection *pgx.Conn) (pgx.Rows, error)
+}
+
+// Inteface of a storage
 type Storage interface {
+	// Create Data in the storage and return its UUID
 	Create(ctx context.Context, data Data) (*uuid.UUID, error)
-	Read(ctx context.Context, data Data) ([]Data, error)
+	// Read Data from the storage by data UUID
+	Read(ctx context.Context, data Data) (Data, error)
+	// Delete Data from the storage by data UUID
 	Delete(ctx context.Context, data Data) error
+	// Return []Data with occurrences of Data search field
+	ReadBySearchField(ctx context.Context, d Data) ([]Data, error)
+}
+
+func checkRequired(d Data) bool {
+	fields := reflect.ValueOf(d).Elem()
+	for i := 0; i < fields.NumField(); i++ {
+		tag := fields.Type().Field(i).Tag.Get("validate")
+		if strings.Contains(tag, "required") && fields.Field(i).IsZero() {
+			return false
+		}
+	}
+	return true
 }
