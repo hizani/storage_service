1f2ccaadb750dd4b15956a45c6417f12d371fd8c
Nikolay Hizanishvili <nikolay.hizanishvili@gmail.com>
2023-01-13 22:31:22 +0300

    + Data interface

--- app/repos/shop.go
+++ app/repos/shop.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 )
 
 var _ Data = &Shop{}
+var _ DbData = &Shop{}
 
 type Shop struct {
 	Id       uuid.UUID `json:"id" validate:"required"`
@@ -15,6 +17,38 @@ type Shop struct {
 	Address  string    `json:"address" validate:"required"`
 	IsClosed bool      `json:"is_closed" validate:"required"`
 	Owner    *string   `json:"owner"`
+}
+
+func (s *Shop) CmpSearchField(data string) bool { return data == s.Name }
+func (s *Shop) CheckRequired() bool             { return checkRequired(s) }
+func (s *Shop) GetId() uuid.UUID                { return s.Id }
+func (s *Shop) GetTypeName() string             { return "shop" }
+func (s *Shop) GetSearchField() string          { return s.Name }
+func (s *Shop) GetSearchFieldName() string      { return "name" }
+func (s *Shop) DbData() DbData                  { return s }
+func (s *Shop) SetDefaults() {
+	if s.Id == uuid.Nil || s.Id.String() == "" {
+		s.Id = uuid.New()
+	}
+}
+func (s *Shop) Insert(ctx context.Context, connection *pgx.Conn) (pgx.Rows, error) {
+	raw, err := connection.Query(ctx, `INSERT INTO shops (id, name, address, is_closed, owner) 
+	values ($1, $2, $3, $4, $5) RETURNING id;`,
+		s.Id, s.Name, s.Address, s.IsClosed, s.Owner)
+	return raw, err
+}
+
+func (c *Shop) SetFieldsFromDbRow(ctx context.Context, row pgx.Rows) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+	}
+	err := row.Scan(&c.Id, &c.Name, &c.Address, &c.IsClosed, &c.Owner)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // storage wrapper
@@ -29,38 +63,38 @@ func NewShops(storage Storage) *Shops {
 func (ss *Shops) Create(ctx context.Context, s Shop) (*uuid.UUID, error) {
 	uid, err := ss.storage.Create(ctx, &s)
 	if err != nil {
-		return nil, fmt.Errorf("create user error: %v", err)
+		return nil, fmt.Errorf("create shop error: %v", err)
 	}
 	return uid, nil
 }
 
 func (ss *Shops) ReadName(ctx context.Context, name string) ([]*Shop, error) {
-	data, err := ss.storage.Read(ctx, &Shop{Name: name})
+	data, err := ss.storage.ReadBySearchField(ctx, &Shop{Name: name})
 	if err != nil {
-		return nil, fmt.Errorf("read user error: %v", err)
+		return nil, fmt.Errorf("read shop error: %v", err)
 	}
 
-	shops := []*Shop{}
-	for _, elem := range data {
-		u, ok := elem.(*Shop)
+	result := []*Shop{}
+	for _, foo := range data {
+		c, ok := foo.(*Shop)
 		if !ok {
-			return nil, fmt.Errorf("read customer error: %v", err)
+			return nil, fmt.Errorf("read shop error: %v", err)
 		}
-		shops = append(shops, u)
+		result = append(result, c)
 	}
 
-	return shops, nil
+	return result, nil
 }
 
 func (ss *Shops) ReadId(ctx context.Context, uid uuid.UUID) (*Shop, error) {
 	data, err := ss.storage.Read(ctx, &Shop{Id: uid})
 	if err != nil {
-		return nil, fmt.Errorf("read user error: %v", err)
+		return nil, fmt.Errorf("read shop error: %v", err)
 	}
 
-	s, ok := data[0].(*Shop)
+	s, ok := data.(*Shop)
 	if !ok {
-		return nil, fmt.Errorf("read customer error: %v", err)
+		return nil, fmt.Errorf("read shop error: %v", err)
 	}
 
 	return s, nil
@@ -69,7 +103,7 @@ func (ss *Shops) ReadId(ctx context.Context, uid uuid.UUID) (*Shop, error) {
 func (ss *Shops) Delete(ctx context.Context, uid uuid.UUID) (*Shop, error) {
 	s, err := ss.storage.Read(ctx, &Shop{Id: uid})
 	if err != nil {
-		return nil, fmt.Errorf("read user error: %v", err)
+		return nil, fmt.Errorf("read shop error: %v", err)
 	}
-	return s[0].(*Shop), ss.storage.Delete(ctx, s[0])
+	return s.(*Shop), ss.storage.Delete(ctx, s)
 }
